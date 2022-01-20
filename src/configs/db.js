d768dbc4213dcb3ebd63f125596d8fb26286933d
Rohit Kumar Singh <93372627+rohitsingh-23@users.noreply.github.com>
2022-01-20 22:02:29 +0530

    Register and login

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports = () => {
-  return mongoose.connect(
-    process.env.databaseLINK,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log("Connected")
-  );
+    return mongoose.connect(
+      process.env.databaseLINK,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      // () => console.log("Connected")
+    );
+    
+  
 };
+
+// module.exports = () => {
+//   try {
+//     return mongoose.connect("mongodb://127.0.0.1:27017/project");
+
+//   } catch (e) {
+//     return console.error(e.message);
+//   }
+// }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 const app = require("./index");
-const server = require("./configs/db");
+const connect = require("./configs/db");
+require("dotenv").config();
+
+const port = process.env.PORT || 2346;
 
 
-
-app.listen(process.env.PORT || 5000, async (req, res) => {
-  server();
-  console.log("Default port is active on PORT");
+app.listen(port, async () => {
+  try {
+    await connect();
+    console.log(`Default port is active on PORT ${port}`);
+  } catch (e) {
+    console.log(e.message);
+  }
 });
